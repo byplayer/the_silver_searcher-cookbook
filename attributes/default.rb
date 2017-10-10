e8366736b4ff86ae2cdd2011bde73c60c943e9c2
byplayer <byplayer100@gmail.com>
2017-10-10 14:44:08 +0800

    update ag version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['the_silver_searcher']['version'] = '2.0.0'
-default['the_silver_searcher']['checksum'] = 'ff7243863f22ed73eeab6f7a6d17cfff585a7eaa41d5ab3ae4f5d6db97701d5f'
+default['the_silver_searcher']['version'] = '2.1.0'
+default['the_silver_searcher']['checksum'] = 'cb416a0da7fe354a009c482ae709692ed567f8e7d2dad4d242e726dd7ca202f0'
 default['the_silver_searcher']['url'] = "https://github.com/ggreer/the_silver_searcher/archive/#{node['the_silver_searcher']['version']}.tar.gz"
 default['the_silver_searcher']['install_dir'] = '/opt/the_silver_searcher'
 default['the_silver_searcher']['build_opt'] = '--prefix=/opt/the_silver_searcher'
