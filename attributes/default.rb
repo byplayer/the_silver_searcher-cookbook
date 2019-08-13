9beab3c77a47a511ec2f80cd2de359e342031273
byplayer <byplayer100@gmail.com>
2019-08-13 14:06:32 +0800

    auto generate download url.
    Because download url need to change every time when user change version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
+
 default['the_silver_searcher']['version'] = '0.31.0'
 default['the_silver_searcher']['checksum'] = '61bc827f4557d8108e91cdfc9ba31632e2568b26884c92426417f58135b37da8'
-default['the_silver_searcher']['url'] = "https://codeload.github.com/ggreer/the_silver_searcher/tar.gz/#{node.the_silver_searcher.version}"
